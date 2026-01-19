ab546c08321b5aee0ce5718d3b85d6e9f1c47900
nmduc <nmduc22112007@gmail.com>
2026-01-19 18:47:05 +0700

    a

---
+++ untitled/nah/abcdefgh.cpp
@@ -0,0 +1,5 @@
+//
+// Created by PC on 1/19/2026.
+//
+
+#include "abcdefgh.h"
---
+++ untitled/nah/abcdefgh.h
@@ -0,0 +1,13 @@
+//
+// Created by PC on 1/19/2026.
+//
+
+#ifndef UNTITLED_ABCDEFGH_H
+#define UNTITLED_ABCDEFGH_H
+
+
+class abcdefgh {
+};
+
+
+#endif //UNTITLED_ABCDEFGH_H
