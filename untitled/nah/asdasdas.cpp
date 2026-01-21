d6c94277022a598be29d984722a53ab9a3bbad78
nmduc <nmduc22112007@gmail.com>
2026-01-21 20:57:27 +0700

    sadfasdfa

--- untitled/nah/asdasdas.cpp
+++ untitled/nah/asdasdas.cpp
@@ -1,5 +1,12 @@
-//
-// Created by PC on 1/19/2026.
-//
+#pragma once
+#include <iostream>
+using namespace std;
+int main() {
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+    return 0;
 
-#include "asdasdas.h"
+}
